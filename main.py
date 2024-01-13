3685259550acd8db456711927b54f6dff814b351
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-01-12 21:43:45 -0800

    Work is Done

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import streamlit as st
+import pickle
+import time
+import langchain
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially openai api key)
+
+st.title("RockyBot: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    
+process_url_clicked = st.sidebar.button("Process URLS")
+
+if process_url.clicked:
+    pass
+
